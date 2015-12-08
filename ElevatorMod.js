a8cd078e51f4cc83422120417e2f7d7df2254f68
Devin Shillington <devin.shillington@hotmail.com>
2015-12-08 15:24:03 -0400

    Update ElevatorMod.js

--- ElevatorMod.js
+++ ElevatorMod.js
@@ -4,6 +4,11 @@
 ModPE.setItem(500, "fireworks", 0, "Thank you! - Papertazer");
 ModPE.setItem(501, "fireworks", 0, "Up Arrow [Player]");
 ModPE.setItem(502, "fireworks", 0, "Down Arrow [Player]");
+ModPE.setItem(503, "fireworks", 0, "Up Arrow [Mob]");
+ModPE.setItem(504, "fireworks", 0, "Down Arrow [Mob]");
+ModPE.setItem(505, "fireworks", 0, "Up Arrow [Item]");
+ModPE.setItem(506, "fireworks", 0, "Down Arrow [Item]");
+ModPE.setItem(507, "fireworks", 0, "Iron Rod");
 
 function useItem(x, y, z, itemId, blockId, side, data){
 if(itemId = 500){
