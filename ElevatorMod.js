bf0acd4e5d40aab29845479d61c532079f7dcda8
Devin Shillington <devin.shillington@hotmail.com>
2015-12-08 12:58:46 -0400

    The start
    the start

---
+++ ElevatorMod.js
@@ -0,0 +1,11 @@
+/*Elevator Mod Mobile*/
+
+ModPE.setItem(500, "fireworks", 0, "Up Arrow [Player]");
+
+function useItem(x, y, z, itemId, blockId, side, data){
+	
+if(itemId = 500){
+	
+clientMessage("You used the up arrow");
+}
+}
