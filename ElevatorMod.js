5c0cf3557d84ed58c696427e8cc35b55fcfabe2d
Devin Shillington <devin.shillington@hotmail.com>
2015-12-08 14:49:11 -0400

    added thank you and down arrow
    thanks and arrow

--- ElevatorMod.js
+++ ElevatorMod.js
@@ -1,11 +1,13 @@
 /*Elevator Mod Mobile*/
+/*Author: papertazer*/
 
-ModPE.setItem(500, "fireworks", 0, "Up Arrow [Player]");
+ModPE.setItem(500, "fireworks", 0, "Thank you! - Papertazer");
+ModPE.setItem(501, "fireworks", 0, "Up Arrow [Player]");
+ModPE.setItem(502, "fireworks", 0, "Down Arrow [Player]");
 
 function useItem(x, y, z, itemId, blockId, side, data){
-	
 if(itemId = 500){
-	
-clientMessage("You used the up arrow");
+clientMessage("Thank you for downloading the Elevator Mod");
 }
+
 }
